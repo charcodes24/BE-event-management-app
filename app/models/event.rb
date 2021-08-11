063e77f469f33cf3826f391df3bf848dd0f43c2b
charcodes24 <charcodes24@gmail.com>
2021-08-11 10:00:06 -0700

    Added class method to Event to sort all events by priority in descending order.

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,10 +6,15 @@ class Event < ActiveRecord::Base
       {
         id: event.id,
         name: event.name,
+        date: event.date,
         priority: event.priority,
         completed: event.completed,
         category: event.category.name
       }
     end
+  end
+
+  def self.sort_by_priority
+    Event.all.order(priority: :desc)
   end
 end
